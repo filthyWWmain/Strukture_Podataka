8f48045ad9d2c01daff54ce4e593e887a87ce9d9
FilthyWWmain <iglicdanijel5@gmail.com>
2023-10-29 21:56:53 +0100

    3. zadatak rjesan ali nekad puca poslje read from file

--- Strukture_2_zadatak/Strukture_2_zadatak/Source.c
+++ Strukture_2_zadatak/Strukture_2_zadatak/Source.c
@@ -6,11 +6,7 @@
 //E.briše odreðeni element iz liste,
 //U zadatku se ne smiju koristiti globalne varijable.
 
-//kad dodaje element na kraj a lista je prazna 
-//kad ide bristali element a taj element je prvi
-//kad input u menu funkciji broj ili string umisto char puca
-//za delete brise samo prvoga po ton prezimenu i ne  porvjerava ime i date_of_birth
-
+//zna crshati nako read from file kad idemo neku funkciju poslje
 
 #define Prolaz_predmeta true
 #define _CRT_SECURE_NO_WARNINGS
@@ -35,6 +31,32 @@ typedef struct _person {
 	personP next;
 }person;
 
+char* input_firstname();
+char* input_lastname();
+int input_date_of_birth();
+personP create_node();
+personP Input_first(personP, personP);
+int print_all_elements(personP);
+personP input_last(personP, personP);
+int find_by_lastname(personP);
+personP delete_person(personP);
+int free_the_memory(personP);
+int menu(personP);
+int add_after_somebody(persoP);
+personP add_before_somebody(personP);
+personP create_node_from_file(char*, char*, int);// isto ko create_node osim sto prima izlaze sa scanf-a
+// stavlja u listu osobe koje su procitane iz datoteke
+personP read_from_file(personP);
+int print_in_file(personP);
+
+int main() {
+	char* name[MAX_LENGTH] = { 0 };
+	personP head = NULL;
+
+	menu(head);
+
+	return 0;
+}
 
 char* input_firstname() {
 
@@ -53,7 +75,7 @@ char* input_lastname() {
 
 	return lastname;
 }
-
+// inputa se uzima kao string pa se onda provjerava eli unos dobar da program ne puca kad se krivo nesto ne unese
 int input_date_of_birth() {
 	char input[MAX_LENGTH] = { 0 };
 	int date_of_birth = 0 ;
@@ -65,11 +87,10 @@ int input_date_of_birth() {
 		scanf(" %s", input);
 		br_pokusaja = br_pokusaja + 1;
 		date_of_birth = 0;
-		for (int i = 0; i < 4; i++) {
-
+		for (int i = 0; i < strlen(input); i++) {
 			if (isdigit(input[i]) != 0) {
-				date_of_birth = date_of_birth + pow(10, 3 - i) * (input[i] - '0');
-				
+				date_of_birth = date_of_birth + pow(10, strlen(input)-1 - i) * (input[i] - '0');
+
 				is_input_ok = true;//stavi da je true svaki put i onda ako je i nakon 4. puta true znaci da su svi uneseni znakovi brojevi
 			}
 			else {
@@ -77,7 +98,7 @@ int input_date_of_birth() {
 				printf("\n\tInput must be a number between 1900 and 2023\n");
 				break;
 			}
-			if ((date_of_birth < 1900 || date_of_birth > 2023) && i == 3) {
+			if ((date_of_birth < 1900 || date_of_birth > 2023) && i == (strlen(input)-1)) {
 				printf("\n\tInput must be a number between 1900 and 2023\n");
 				is_input_ok = false;
 			}
@@ -85,8 +106,8 @@ int input_date_of_birth() {
 		}
 	}
 	if(br_pokusaja == 3 && is_input_ok == false){
-		printf("\n\tSmotan si ko sajla. Staviti cu ti da si 2002. godiste\n");
-		date_of_birth = 2002;
+		printf("\n\tSmotan si ko sajla. Staviti cu ti da si 2000. godiste\n");
+		date_of_birth = 2000;
 	}
 	return date_of_birth;
 	
@@ -140,11 +161,12 @@ int print_all_elements(personP head) {
 
 //stavlja na zadnje misto novog clana
 personP input_last(personP head, personP q) {
-	if (head) {
-		while (head->next != NULL) {
-			head = head->next;
+	personP current = head;
+	if (current) {
+		while (current->next != NULL) {
+			current = current->next;
 		}
-		head->next = q;
+		current->next = q;
 	}
 	else {
 		return q;
@@ -180,13 +202,13 @@ personP delete_person(personP head) {
 	personP prev_temp = NULL;//pokazuje na clana ispred clana temp u listi, da moremo izbrisati clan temp
 	char firstname[MAX_LENGTH] = { 0 };
 	char lastname[MAX_LENGTH] = { 0 };
-	int date_of_birth = 0 ;
+	int date_of_birth = 0;
 	bool is_first = true;
 
 	strcpy(firstname, input_firstname());
 	strcpy(lastname, input_lastname());
 	date_of_birth = input_date_of_birth();
-
+	
 	while (temp && strcmp(temp->firstname, firstname) != 0 && strcmp(temp->lastname, lastname) != 0 && temp->date_of_birth == date_of_birth) {
 
 		is_first = false;
@@ -226,40 +248,151 @@ int free_the_memory(personP head) {
 		printf("\n\tDeleting : %s %s %d", to_delete->firstname, to_delete->lastname, to_delete->date_of_birth);
 		free(to_delete);
 	}
+	return EXIT_SUCCESS;
+}
+
+int add_after_somebody(personP head) {
+
+	char to_find_lastname[MAX_LENGTH] = { 0 };
+	personP temp = NULL;
+	printf("\n\tInput lastname of the person you are looking for:");
+	strcpy(to_find_lastname, input_lastname());
+
+	while (strcmp(head->lastname, to_find_lastname) != 0) {
+		if (head->next == NULL) {
+
+			printf("\n\tNo person with that last name");
+			return 0;
+		}
+
+		head = head->next;
+	}
+	temp = create_node();
+	temp->next = head->next;
+	head->next = temp;
+
+	return EXIT_SUCCESS;
+}
+personP add_before_somebody(personP head) {
+	personP before_current = NULL;
+	personP current = head;
+	char to_find_lastname[MAX_LENGTH] = { 0 };
+	personP temp = NULL;
+
+	printf("\n\tInput lastname of the person you are looking for:");
+	strcpy(to_find_lastname, input_lastname());
+
+	while (strcmp(current->lastname, to_find_lastname) != 0) {
+		if (current->next == NULL) {
+
+			printf("\n\tNo person with that last name");
+			return 0;
+		}
+		before_current = current;
+		current = current->next;
+	}
+	temp = create_node();
+	temp->next = current;// stavlja da je poslje tempa current, ako nema clana isprid njega onda vracamo temp jer je to novi head
+	if (before_current == NULL) {
+		return temp;
+	}
+	//inace stavljamo da je temp nakon current_before
+	else {
+		before_current->next = temp;
+		return head;
+	}
+}
+
+
+personP read_from_file(personP head) {
+
+	char firstname[MAX_LENGTH] = { 0 };
+	char lastname[MAX_LENGTH] = { 0 };
+	int year_of_birth = 0;
+	FILE* fp = NULL;
+	//int test  =0;
+	fp = fopen("filename.txt", "r");
+	if (fp == NULL) {
+		return FILE_NOT_OPENED;
+	}
+	//while (test != EOF) {
+	//	test = fscanf(fp, " %s %s %d", firstname, lastname, &year_of_birth);
+	//	head = Input_first(head, create_node_from_file(firstname, lastname, year_of_birth));
+	// more i ovako provjera EOF je int;
+
+	while (fscanf(fp," %s %s %d", firstname, lastname, &year_of_birth) != EOF) {
+		head = Input_first(head, create_node_from_file(firstname, lastname, year_of_birth));
+	}
+	fclose(fp);
+
+	return head;
+}
+
+personP create_node_from_file(char* firstname, char* lastname, int date_of_birth) {
+	personP q = NULL;
+	q = (personP)malloc(sizeof(personP));
+	if (!q) {
+		perror("\tCan't allocate memory");
+		return NULL;
+	}
+	strcpy(q->firstname, firstname);
+	strcpy(q->lastname, lastname);
+	q->date_of_birth = date_of_birth;
+	q->next = NULL;
+	return q;
+}
+
+int print_in_file(personP head) {
+	
+	personP current = head;
+	FILE* fp = NULL;
+	fp = fopen("printedlist.txt", "w");
+	if (fp == NULL) {
+		return FILE_NOT_OPENED;
+	}
+	while (current) {
+		fprintf(fp," %s %s %d\n",current->firstname,current->lastname,current->date_of_birth);
+		current = current->next;
+	}
+	fclose(fp);
+
+	return EXIT_SUCCESS;
 }
 
 int menu(personP head) {
 	char input[MAX_LENGTH] = { 0 };
-	int answer = 0;
+	int answer = 10;
 	char garbage_colection[1024] = { 0 };
-	while (answer != 1) {
+	while (answer != 0) {
 		printf("\n*********************************************************\n");
-		printf("\n\tKill program (1) \n");
-		printf("\tInput person at the start of list (2) \n");
-		printf("\tPrint all (3) \n");
-		printf("\tInput person at the end of list(4) \n");
-		printf("\tSearch_by_lastname (5) \n");
-		printf("\tDelete a person from the list (6) \n");
+		printf("\n\tKill program (0) \n");
+		printf("\tInput person at the start of list (1) \n");
+		printf("\tPrint all (2) \n");
+		printf("\tInput person at the end of list(3) \n");
+		printf("\tSearch_by_lastname (4) \n");
+		printf("\tDelete a person from the list (5) \n");
+		printf("\tAdd new person after someone (6) \n");
+		printf("\tAdd new person before someone (7) \n");
+		printf("\tAdd new people from a file (8) \n");
+		printf("\tPrint list in a file(9) \n");
 		printf("\n*********************************************************\n");
 
 		printf("\n\tInput: ");
 		scanf(" %[^\n]s", input);
 		// provjera eli unesen broj a ne char
-		if (isdigit(input[0]) == 0 || input[0] - '0' < 1 || input[0] - '0' > 6) {
+		if (isdigit(input[0]) == 0 || input[0] - '0' < 0 || input[0] - '0' > 9) {
 			printf("\n*********************************************************\n\n");
 			printf("\tInvalid Input,try again\n");
-
-			//scanf(" %s", garbage_colection);
 
 		}
 		else {
 
 			answer = input[0] - '0'; // pretvara char u int
 
-			if (answer == 2) {
+			if (answer == 1) {
 				head = Input_first(head, create_node());
 			}
-			else if (answer == 3) {
+			else if (answer == 2) {
 				if (!head) {
 					printf("\n\tNothing to see here, listi is empty\n");
 				}
@@ -268,10 +401,10 @@ int menu(personP head) {
 				}
 				
 			}
-			else if (answer == 4) {
+			else if (answer == 3) {
 				head = input_last(head, create_node());
 			}
-			else if (answer == 5) {
+			else if (answer == 4) {
 				if (!head) {
 					printf("\n\tNothing to search here, listi is empty\n");
 				}
@@ -279,7 +412,7 @@ int menu(personP head) {
 					find_by_lastname(head);
 				}
 			}
-			else if (answer == 6) {
+			else if (answer == 5) {
 				if (!head) {
 					printf("\n\tNothing to delete here, listi is empty\n");
 				}
@@ -288,25 +421,40 @@ int menu(personP head) {
 				}
 
 			}
+			else if(answer == 6) {
+				if (!head) {
+					printf("\n\tYou can't add after somebody if the list is empty!\n");
+				}
+				else {
+					add_after_somebody(head);
+				}
+				
+			}
+			else if (answer == 7) {
+				if (!head) {
+					printf("\n\tYou can't add before somebody if the list is empty!\n");
+				}
+				else {
+					head= add_before_somebody(head);
+				}
+
+			}
+			else if (answer == 8) {
+				head = read_from_file(head);
+			}
+			else if (answer == 9) {
+				print_in_file(head);
+			}
+
 		}
 
 	}
 
-	free_the_memory(head);
+	//free_the_memory(head);
 
 	printf("\n \n \n**************************************\n \n");
 	printf("\tProgram killed");
 	printf("\n \n \n**************************************\n \n");
-
-
-	return 0;
-}
-
-int main() {
-	char* name[MAX_LENGTH] = { 0 };
-	personP head = NULL;
-
-	menu(head);
 
 
 	return 0;
